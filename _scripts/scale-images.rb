39c0347fc4e73b37c3a67d1f513db5f90cb2a8db
Anders Lemke <mail@anderslemke.dk>
2013-04-24 13:53:15 +0200

    New name convention for image names

--- _scripts/scale-images.rb
+++ _scripts/scale-images.rb
@@ -17,16 +17,14 @@ photographers.each do |photographer|
 
   images = Dir.entries(photographer_dir).select {|entry| !entry.match(/^\./) }
 
-  i = 0
   images.each do |image_name|
-    i += 1
     image_path = File.join(photographer_dir, image_name)
     image = Magick::Image::read(image_path).first
     thumb = image.resize_to_fit(200, 200)
     small = image.resize_to_fit(500, 500)
     big = image.resize_to_fit(1000, 1000)
     
-    filename = "#{photographer}-#{i}.#{image.format.downcase}"
+    filename = "#{photographer}-#{File.basename(image.filename)}"
     thumb.write File.join(thumb_dir, filename)
     small.write File.join(small_dir, filename)
     big.write File.join(big_dir, filename)
